c7e9388944e5f7674ec631434994bb89af9a6512
imane0203 <zekkaouiimane@gmail.com>
2021-06-14 00:06:05 +0100

    team

---
+++ back-end/routes/records.js
@@ -0,0 +1,65 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /ville.
+const recordRoutes = express.Router();
+
+//This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This section will help you get a list of all the cities.
+recordRoutes.route("/ville").get(function (req, res) {
+  let db_connect = dbo.getDb("cities");
+  db_connect
+    .collection("cities")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// // This section will help you create a new record.
+// recordRoutes.route("/ville/add").post(function (req, res) {
+//   let db_connect = dbo.getDb("cities");
+//   let myobj = {
+//     person_name: req.body.person_name,
+//     person_position: req.body.person_position,
+//     person_level: req.body.person_level,
+//   };
+//   db_connect.collection("cities").insertOne(myobj, function (err, res) {
+//     if (err) throw err;
+//   });
+// });
+
+// // This section will help you update a record by id.
+// recordRoutes.route("/update/:id").post(function (req, res) {
+//   let db_connect = dbo.getDb("cities");
+//   let myquery = { id: req.body.id };
+//   let newvalues = {
+//     $set: {
+//       person_name: req.body.person_name,
+//       person_position: req.body.person_position,
+//       person_level: req.body.person_level,
+//     },
+//   };
+//   db_connect
+//     .collection("cities")
+//     .updateOne(myquery, newvalues, function (err, res) {
+//       if (err) throw err;
+//       console.log("1 document updated");
+//     });
+// });
+
+// // This section will help you delete a record
+// recordRoutes.route("/:id").delete((req, res) => {
+//   let db_connect = dbo.getDb("cities");
+//   var myquery = { id: req.body.id };
+//   db_connect.collection("cities").deleteOne(myquery, function (err, obj) {
+//     if (err) throw err;
+//     console.log("1 document deleted");
+//   });
+// });
+
+module.exports = recordRoutes;
